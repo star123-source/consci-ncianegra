faa9ac9b696572d30d73c96423526452af4d3c8b
star123-source <goncalves.paula.beatriz@escola.pr.gov.br>
2025-11-04 00:40:27 -0300

    up

--- app4.js
+++ app4.js
@@ -4,12 +4,12 @@ let foto_troca = false;
 function TrocaFoto() {
   if (foto_troca === false) {
     for (let i = 0; i < imagem.length; i++) {
-      imagem[i].src = `pedro${i + 9}.jpg`;
+  imagem[i].src = `machado${i + 2}.jpg`;
     }
     foto_troca = true;
   } else {
     for (let i = 0; i < imagem.length; i++) {
-      imagem[i].src = `pedro${i}.jpg`;
+      imagem[i].src = `m achadodeassis${i}.jpg`;
     }
     foto_troca = false;
   }
